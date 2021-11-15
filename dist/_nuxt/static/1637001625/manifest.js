f98076e3e2887b8e811ba75385a2b77d154e1126
Niyuby Blanco <nblanco.nb17@gmail.com>
2021-11-15 14:33:18 -0430

    Portfolio - Padding

--- dist/_nuxt/static/1637001625/manifest.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("manifest.js", {routes:["\u002Fcontacto","\u002Facercade","\u002Fportfolio","\u002F"]})
---
+++ dist/_nuxt/static/1637002945/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Facercade","\u002F","\u002Fcontacto","\u002Fportfolio"]})
