d8296fa7d3bd977f254bfab49511507c5d243f7e
Niyuby Blanco <nblanco.nb17@gmail.com>
2021-12-10 19:06:31 -0430

    Portfolio - Language

---
+++ dist/_nuxt/static/1639179239/contacto/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c,d){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1639179239",layout:"default",error:a,state:{nav:{toggleSidebar:b}},serverRendered:c,routePath:"\u002Fcontacto",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}},colorMode:{preference:d,value:d,unknown:c,forced:b}}}(null,false,true,"system"));
---
+++ dist/_nuxt/static/1639179239/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fportfolio","\u002Fcontacto","\u002Facercade","\u002F"]})
